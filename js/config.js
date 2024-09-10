b35216ab95fbc11e290546e47cd32e3d887d2511
David-Paul Adams <david-paul.adams@outlook.de>
2024-09-10 15:25:36 +0200

    edited the config file

--- js/config.js
+++ js/config.js
@@ -1,4 +1,43 @@
-function is_Checkbox_Checked(x)
+// changes the class from the corresponding label
+function changeDropdownIcon_Language()
 {
-    x.classList.toggle("fa-angle-up")
+    var element = document.querySelector("#icon_languages")
+
+    if(element.classList == "icon_before")
+    {
+        element.classList.replace("icon_before","icon_after")
+    }
+    else if(element.classList == "icon_after")
+    {
+        element.classList.replace("icon_after","icon_before")
+    }
 }
+
+function changeDropdownIcon_Tools()
+{
+    var element = document.querySelector("#icon_tools")
+
+    if(element.classList == "icon_before")
+    {
+        element.classList.replace("icon_before","icon_after")
+    }
+    else if(element.classList == "icon_after")
+    {
+        element.classList.replace("icon_after","icon_before")
+    }
+}
+
+function changeDropdownIcon_Dlc()
+{
+    var element = document.querySelector("#icon_dlc")
+
+    if(element.classList == "icon_before")
+    {
+        element.classList.replace("icon_before","icon_after")
+    }
+    else if(element.classList == "icon_after")
+    {
+        element.classList.replace("icon_after","icon_before")
+    }
+}
+
